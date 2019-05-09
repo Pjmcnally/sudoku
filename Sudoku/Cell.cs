51559d258560251baf8df3a2889be2d3e9d291ed
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-05-09 01:02:48 -0500

    flesh out cell class

--- Sudoku/Cell.cs
+++ Sudoku/Cell.cs
@@ -10,36 +10,58 @@ namespace Sudoku
     {
         public int Row { get; }
         public int Col { get; }
-        public int Box { get; }
+        public int Box { get; private set; }
         public int Value { get; private set; }
         public bool Solved { get; private set; }
 
-        public Cell(int row, int col, int box, int value)
+        // TODO: Add property to track possible guesses (HashSet)
+        // TODO: Add property to track peers
+        // TODO: Add reset method
+        // TODO: Add method for constraint propagation
+        // TODO: Add method to update peers
+
+        public Cell(int row, int col, int value)
         {
+            // Check for invalid values and throw exception.
+            if (row < 0 || row > 8)
+            {
+                throw new System.ArgumentException("Parameter must be integer 0 - 8", "Row");
+            }
+            else if (col < 0 || col > 8)
+            {
+                throw new System.ArgumentException("Parameter must be integer 0 - 8", "Col");
+            }
+            else if (value < 0 || value > 9)
+            {
+                throw new System.ArgumentException("Parameter must be integer 0 - 9", "Value");
+            }
+
+            // Save values 
             Row = row;
             Col = col;
-            Box = box;
             Value = value;
 
             // If a value is provided at instantiation the cell is already solved.
             if (value > 0)
             {
-                this.Solved = true;
+                Solved = true;
             }
+
+            // Calculate which 3x3 box the cell is in (listed from 0-8)
+            // Where each number below represents a 3x3 group of cells.
+            // 0 1 2
+            // 3 4 5
+            // 6 7 8
+            Box = (Row / 3) + ((Col / 3) * 3);
         }
 
         public bool UpdateValue()
         {
             // Increase value to next available value if value exceeds max (9) set value to 0 (unknown)
-            Value++;
-            if (Value > 9)
-            {
-                Value = 0; 
-            }
+            Value = (Value + 1) % 10;
 
-            // Return false if 0. True if any other number.
+            // Return true if any number other than 0. Otherwise return false.
             return Convert.ToBoolean(Value);
-
         }
     }
 }
