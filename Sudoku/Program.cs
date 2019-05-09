4a8c8465087fccc3d1ced13535f628e8aeea50ea
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-05-09 01:03:46 -0500

    update program

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -12,11 +12,14 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            Sudoku game = new Sudoku();
-            game.GetBoard(Sudoku.Difficulty.random);
+            SudokuBoard game = new SudokuBoard();
+            game.GetBoard(SudokuBoard.Difficulty.random);
             game.DisplayBoard();
             game.GetSolution();
             game.DisplaySolution();
+
+            Cell test = new Cell(5, 5, 9);
+            Console.WriteLine($"Row: {test.Row}\nCol: {test.Col}\nBox: {test.Box}\nValue: {test.Value}\nSolved: {test.Solved}");
         }
     }
 }
