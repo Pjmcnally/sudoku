408aea76193f33aa242a872bb7c252158dcb3faf
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-05-10 02:01:49 -0500

    Improve debug display

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -15,11 +15,12 @@ namespace Sudoku
             SudokuBoard game = new SudokuBoard();
             game.GetBoard(SudokuBoard.Difficulty.random);
             game.DisplayBoard();
+            Console.WriteLine();
             game.GetSolution();
             game.DisplaySolution();
 
             Cell test = new Cell(5, 5, 9);
-            Console.WriteLine($"Row: {test.Row}\nCol: {test.Col}\nBox: {test.Box}\nValue: {test.Value}\nSolved: {test.Solved}");
+            Console.WriteLine($"\nCell Created:\nRow: {test.Row}\nCol: {test.Col}\nBox: {test.Box}\nValue: {test.Value}\nSolved: {test.Solved}");
         }
     }
 }
