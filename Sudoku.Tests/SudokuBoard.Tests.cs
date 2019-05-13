d467e60e91945b4decf329294b124a90ab4ac711
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-05-13 01:59:35 -0500

    Make test work with complex parameters

--- Sudoku.Tests/SudokuBoard.Tests.cs
+++ Sudoku.Tests/SudokuBoard.Tests.cs
@@ -28,14 +28,19 @@ namespace Sudoku.Tests
     public class BackTrackSolve
     {
         [Theory]
-        [InlineData(true, "[[0,0,0,0,0,0,0,0,2],[0,0,0,4,0,0,7,0,9],[4,0,0,0,7,0,1,3,5],[0,1,4,0,0,5,0,0,0],[0,5,8,0,0,1,2,0,4],[7,0,0,0,0,0,0,5,1],[6,3,0,5,4,0,0,0,8],[0,7,0,9,1,0,0,0,6],[0,4,2,6,0,7,5,0,0]]")]
-        [InlineData(false, "[[1,1,1,1,1,1,1,1,1],[0,0,0,4,0,0,7,0,9],[4,0,0,0,7,0,1,3,5],[0,1,4,0,0,5,0,0,0],[0,5,8,0,0,1,2,0,4],[7,0,0,0,0,0,0,5,1],[6,3,0,5,4,0,0,0,8],[0,7,0,9,1,0,0,0,6],[0,4,2,6,0,7,5,0,0]]")]
-        public void ReturnValue(bool expectedReturnValue, string board)
+        [MemberData(nameof(ReturnValueData))]
+        public void ReturnValue(bool expected, SudokuBoard game)
         {
-            SudokuBoard testBoard = new SudokuBoard(board);
-            Assert.Equal(expectedReturnValue, testBoard.BacktrackSolve());
+            Assert.Equal(expected, game.BacktrackSolve());
         }
+
+        public static IEnumerable<object[]> ReturnValueData => new List<object[]>
+        {
+            new object[] { true, new SudokuBoard("[[0,0,0,0,0,0,0,0,2],[0,0,0,4,0,0,7,0,9],[4,0,0,0,7,0,1,3,5],[0,1,4,0,0,5,0,0,0],[0,5,8,0,0,1,2,0,4],[7,0,0,0,0,0,0,5,1],[6,3,0,5,4,0,0,0,8],[0,7,0,9,1,0,0,0,6],[0,4,2,6,0,7,5,0,0]]") },
+            new object[] { false, new SudokuBoard("[[1,1,1,1,1,1,1,1,1],[0,0,0,4,0,0,7,0,9],[4,0,0,0,7,0,1,3,5],[0,1,4,0,0,5,0,0,0],[0,5,8,0,0,1,2,0,4],[7,0,0,0,0,0,0,5,1],[6,3,0,5,4,0,0,0,8],[0,7,0,9,1,0,0,0,6],[0,4,2,6,0,7,5,0,0]]") },
+        };
 
         // TODO: Write tests to compare actual solution
     }
-}   
+}
+
