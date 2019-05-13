5ac59cad81d9c007513343dcab4500cd506ecbe6
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-05-13 01:59:43 -0500

    Add example to program

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -16,6 +16,7 @@ namespace Sudoku
             // game.DisplayBoard();
             // Console.WriteLine();
             // game.DisplaySolution();
+            game = new SudokuBoard("[[1,1,1,1,1,1,1,1,1],[0,0,0,4,0,0,7,0,9],[4,0,0,0,7,0,1,3,5],[0,1,4,0,0,5,0,0,0],[0,5,8,0,0,1,2,0,4],[7,0,0,0,0,0,0,5,1],[6,3,0,5,4,0,0,0,8],[0,7,0,9,1,0,0,0,6],[0,4,2,6,0,7,5,0,0]]");
 
             Cell test = new Cell(5, 5, 9);
             Console.WriteLine($"\nCell Created:\nRow: {test.Row}\nCol: {test.Col}\nBox: {test.Box}\nValue: {test.Value}\nSolved: {test.Solved}");
