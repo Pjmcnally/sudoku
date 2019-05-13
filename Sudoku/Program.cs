e19a0dbd71062188a0ae0dd871e2bfc0ea82a171
Pjmcnally <Patrick.J.McNally@gmail.com>
2019-05-13 01:12:57 -0500

    Remove GetSolution method

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Sudoku
 {
@@ -9,13 +10,11 @@ namespace Sudoku
             SudokuBoard game = new SudokuBoard(SudokuBoard.Difficulty.random);
             // game.DisplayBoard();
             // Console.WriteLine();
-            game.GetSolution();
             // game.DisplaySolution();
 
             game = new SudokuBoard("[[0,0,0,0,0,0,0,0,2],[0,0,0,4,0,0,7,0,9],[4,0,0,0,7,0,1,3,5],[0,1,4,0,0,5,0,0,0],[0,5,8,0,0,1,2,0,4],[7,0,0,0,0,0,0,5,1],[6,3,0,5,4,0,0,0,8],[0,7,0,9,1,0,0,0,6],[0,4,2,6,0,7,5,0,0]]");
             // game.DisplayBoard();
             // Console.WriteLine();
-            game.GetSolution();
             // game.DisplaySolution();
 
             Cell test = new Cell(5, 5, 9);
